7702d82a39733dd49606b26954a1e3231ddf0201
Colin Ronayne <cronayne@gmail.com>
2020-04-21 17:11:18 +0200

    Created First Screen and Route to Second

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 60.0;
+const kActiveCardColour = Color(0XFF1D1E33);
+const kInactiveCardColour = Color(0XFF111328);
+const kBottomContainerColour = Color(0XFFEB1555);
+const kMinHeight = 120.0;
+const kMaxHeight = 220.0;
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0XFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+    fontSize: 50.0,
+    fontWeight: FontWeight.w900,
+);
